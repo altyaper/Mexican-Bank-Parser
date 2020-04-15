6c2e48dfb6266461ac3ffec14c7d35e7c9829aba
Jorge Chavez <jorge.chavez@points.com>
2020-04-14 22:58:59 -0700

    Implement parse for banamex

---
+++ README.md
@@ -0,0 +1,48 @@
+# ParserBankMx
+
+Usage:
+
+```javascript
+import { ParserBanamex } from 'parserbankmx'
+
+const myFileContent = '
+"Fecha","Descripción","Débito","Crédito","Saldo","Moneda"
+"05-01-2018","NOUR BISTRO REST         BNO 17012SEBAMX ","7,629.10","","0.00","MXN"
+"05-01-2018","REST LICORERIA CENTRAL   GFI 141204SC1MX ","1,558.25","","0.00","MXN"
+"05-01-2018","PAYPAL *YOURLAPTOPS      4029357722   LU ","206.18","","0.00","MXN"
+"06-01-2018","PAYPAL *SALESCTE2        4029357711   LU ","4,087.55","","0.00","MXN"
+';
+
+const parser = new ParserBanamex(myFileContent);
+
+console.log(parser.parse());
+```
+
+Output:
+```
+[ { date: 2018-06-01T07:00:00.000Z,
+    reference: 'PAYPAL *SALESCTE2        4029357711   LU ',
+    payment: '4,087.55',
+    charge: '0',
+    balance: '0.00',
+    currency: 'MXN' },
+  { date: 2018-05-01T07:00:00.000Z,
+    reference: 'PAYPAL *YOURLAPTOPS      4029357722   LU ',
+    payment: '206.18',
+    charge: '0',
+    balance: '0.00',
+    currency: 'MXN' },
+  { date: 2018-05-01T07:00:00.000Z,
+    reference: 'REST LICORERIA CENTRAL   GFI 141204SC1MX ',
+    payment: '1,558.25',
+    charge: '0',
+    balance: '0.00',
+    currency: 'MXN' },
+  { date: 2018-05-01T07:00:00.000Z,
+    reference: 'NOUR BISTRO REST         BNO 17012SEBAMX ',
+    payment: '7,629.10',
+    charge: '0',
+    balance: '0.00',
+    currency: 'MXN' } ]
+```
+
--- test/parser.test.js
+++ test/parser.test.js
@@ -14,41 +14,57 @@ describe('Parser', () => {
     });
   });
 
-  it('should convert content into an array', () => {
-    expect(parser.split()).to.be.an('array');
-  });
 
-  it('should remove the first line from the array', () => {
-    const splitted = parser.split();
-    const noFirstLine = parser.removeFirstLine(splitted);
-    expect(noFirstLine).to.be.an('array');
-  });
-
-  it('should split line as Banamex line', () => {
-    const line = '"05-01-2018","NOUR BISTRO REST         BNO 17012SEBAMX ","7,629.10","","0.00","MXN"\r';
-    const object = parser.getPaymentObject(line);
-    expect(object[1]).to.be.eql('05-01-2018');
-    expect(object[2]).to.be.eql('NOUR BISTRO REST         BNO 17012SEBAMX ');
-    expect(object[3]).to.be.eql('7,629.10');
-    expect(object[4]).to.be.eql('');
-    expect(object[5]).to.be.eql('0.00');
-    expect(object[6]).to.be.eql('MXN');
-  });
-
-  it('should get an array of payment', () => {
-    const splitted = parser.split();
-    const noFirstLine = parser.removeFirstLine(splitted);
-    const payments = parser.getArrayPaymentsObject(noFirstLine);
-    expect(payments).to.be.an('array');
-    expect(payments[0]).to.be.an('object');
-    expect(payments[0]).to.be.eql({
-      date: new Date('06-01-2018'),
-      reference: 'PAYPAL *SALESCTE2        4029357711   LU ',
-      payment: '4,087.55',
-      charge: '0',
-      balance: '0.00',
-      currency: 'MXN'
+  describe('ParserBanamex', () => {
+    it('should convert content into an array', () => {
+      expect(parser.split()).to.be.an('array');
     });
+
+    it('should remove the first line from the array', () => {
+      const splitted = parser.split();
+      const noFirstLine = parser.removeFirstLine(splitted);
+      expect(noFirstLine).to.be.an('array');
+    });
+
+    it('should split line as Banamex line', () => {
+      const line = '"05-01-2018","NOUR BISTRO REST         BNO 17012SEBAMX ","7,629.10","","0.00","MXN"\r';
+      const object = parser.getPaymentObject(line);
+      expect(object[1]).to.be.eql('05-01-2018');
+      expect(object[2]).to.be.eql('NOUR BISTRO REST         BNO 17012SEBAMX ');
+      expect(object[3]).to.be.eql('7,629.10');
+      expect(object[4]).to.be.eql('');
+      expect(object[5]).to.be.eql('0.00');
+      expect(object[6]).to.be.eql('MXN');
+    });
+
+    it('should get an array of payment', () => {
+      const splitted = parser.split();
+      const noFirstLine = parser.removeFirstLine(splitted);
+      const payments = parser.getArrayPaymentsObject(noFirstLine);
+      expect(payments).to.be.an('array');
+      expect(payments[0]).to.be.an('object');
+      expect(payments[0]).to.be.eql({
+        date: new Date('05-01-2018'),
+        reference: 'NOUR BISTRO REST         BNO 17012SEBAMX ',
+        payment: '7,629.10',
+        charge: '0',
+        balance: '0.00',
+        currency: 'MXN'
+      });
+    });
+
+    it('should parser a content correctly', () => {
+      const final = parser.parse();
+      expect(final[final.length - 1]).to.be.eql({
+        date: new Date('2018-05-01T07:00:00.000Z'),
+        reference: 'NOUR BISTRO REST         BNO 17012SEBAMX ',
+        payment: '7,629.10',
+        charge: '0',
+        balance: '0.00',
+        currency: 'MXN'
+      });
+    });
+
   });
 
 });
